77663025c93a5345c10e07bf678ccb169c6bc7d2
Vlad <v-kucher@bk.ru>
2024-07-06 15:09:15 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_dz = '12'
+number_time = '1.5'
+name = 'Python'
+time_for_1_task = 1.5/12
+print("Курс:",name,"всего задач:",number_dz,"затраченно часов:",number_time," среднее время выполнения:",time_for_1_task)
