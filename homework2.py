863a990a2e551499c2ff23eac7764f0d8d6208bf
Vlad <v-kucher@bk.ru>
2024-07-08 14:23:42 +0300

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -1,5 +1,5 @@
 number_dz = '12'
 number_time = '1.5'
 name = 'Python'
-time_for_1_task = 1.5/12
+time_for_1_task = float(number_time) /int(number_dz)
 print("Курс:",name,"всего задач:",number_dz,"затраченно часов:",number_time," среднее время выполнения:",time_for_1_task)
